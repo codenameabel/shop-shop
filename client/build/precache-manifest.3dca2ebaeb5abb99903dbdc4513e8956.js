2675000c9818dcf9d2741b2f33e385194e161633
codenameabel <newabelmartinez@gmail.com>
2021-08-29 16:01:04 -0700

    redux installed again

---
+++ client/build/precache-manifest.3dca2ebaeb5abb99903dbdc4513e8956.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dd42124e878c21aec97311cf0e43742",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2c46a8cdc6390e63472",
+    "url": "/static/css/main.50cc2ee3.chunk.css"
+  },
+  {
+    "revision": "ff7a52b74d0cc6b0d8e3",
+    "url": "/static/js/2.3b712662.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.3b712662.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2c46a8cdc6390e63472",
+    "url": "/static/js/main.40ef7f27.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
